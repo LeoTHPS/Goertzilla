b8a8f2ab570105d536ab76dc204c39c496d5f590
= <=>
2025-12-24 10:33:18 -0800

    refactor

--- demo.cpp
+++ demo.cpp
@@ -9,17 +9,21 @@
 
 int16_t samples[SAMPLE_COUNT] = {};
 
-int main(int argc, char* argv[])
+void samples_init()
 {
 	double f[] = { 697,  1209, 100  };
 	double a[] = { 0.25, 0.25, 0.25 };
 
 	Goertzilla::GenerateSineWave(samples, SAMPLE_COUNT, SAMPLE_RATE, CHANNEL_INDEX, CHANNEL_COUNT, f, a);
+	Goertzilla::Window(samples, SAMPLE_COUNT, CHANNEL_INDEX, CHANNEL_COUNT, GOERTZILLA_WINDOW_HAMMING | GOERTZILLA_WINDOW_SYMMETRIC);
+}
+
+int main(int argc, char* argv[])
+{
+	samples_init();
 
 	char   dtmf;  double dtmf_magnitude;
 	double ctcss; double ctcss_magnitude;
-
-	Goertzilla::Window(samples, SAMPLE_COUNT, CHANNEL_INDEX, CHANNEL_COUNT, GOERTZILLA_WINDOW_HAMMING | GOERTZILLA_WINDOW_SYMMETRIC);
 
 	dtmf  = Goertzilla::DTMF(samples, SAMPLE_COUNT, SAMPLE_RATE, CHANNEL_INDEX, CHANNEL_COUNT, dtmf_magnitude);
 	ctcss = Goertzilla::CTCSS(samples, SAMPLE_COUNT, SAMPLE_RATE, CHANNEL_INDEX, CHANNEL_COUNT, ctcss_magnitude);

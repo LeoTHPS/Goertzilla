fcc4a23f4b190564d51fff685a6f96ba7b4ecd77
= <=>
2026-01-23 11:01:24 -0800

    refactor

--- Goertzilla.hpp
+++ Goertzilla.hpp
@@ -37,7 +37,8 @@ class Goertzilla
 	static constexpr double PI  = 3.14159265358979323846;
 	static constexpr double PI2 = PI * 2;
 
-	static constexpr char   DTMF_KEY[4][4]        = { { '1', '2', '3', 'A' }, { '4', '5', '6', 'B' }, { '7', '8', '9', 'C' }, { '*', '0', '#', 'D' } };
+	static constexpr char   DTMF_KEY[4][4]        = { { '1', '2', '3', 'A' }, { '4', '5', '6', 'B'}, { '7', '8', '9', 'C'}, { '*', '0', '#', 'D' } };
+	static constexpr size_t DTMF_KEY_COUNT        = sizeof(DTMF_KEY) / sizeof(char);
 	static constexpr double DTMF_FREQUENCY[8]     = { 697, 770, 852, 941, 1209, 1336, 1477, 1633 };
 	static constexpr size_t DTMF_FREQUENCY_COUNT  = sizeof(DTMF_FREQUENCY) / sizeof(double);
 
@@ -71,8 +72,8 @@ public:
 
 			if (frequency[j] > frequency_max_magnitude[1])
 			{
-				frequency_max_magnitude[1]  = frequency[j];
-				frequency_max_offset[1] = j;
+				frequency_max_offset[1]    = j;
+				frequency_max_magnitude[1] = frequency[j];
 			}
 		}
 
@@ -99,8 +100,8 @@ public:
 
 			if (frequency[j] > frequency_max_magnitude[1])
 			{
-				frequency_max_magnitude[1]  = frequency[j];
-				frequency_max_offset[1] = j;
+				frequency_max_offset[1]    = j;
+				frequency_max_magnitude[1] = frequency[j];
 			}
 		}
 
